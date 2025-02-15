22cc22031397f35939f4686cd26d8cfd947d5a64
Aaron Campos <aaroncm611@gmail.com>
2025-02-16 00:15:07 +0100

    cambiamos el firebase.js

--- src/db/firebase.js
+++ src/db/firebase.js
@@ -4,12 +4,12 @@ import { getFirestore} from "firebase/firestore"
 
 
 const firebaseConfig = {
-    apiKey: process.env.apiKey,
-    authDomain: process.env.authDomain,
-    projectId: process.env.projectId,
-    storageBucket: process.env.storageBucket,
-    messagingSenderId: process.env.messagingSenderId,
-    appId: process.env.appId
+    apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+    authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.VUE_APP_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
