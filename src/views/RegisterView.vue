e4a6a95d5e5d557109a097d03c12e4c4a2a50d87
Aaron Campos <aaroncm611@gmail.com>
2025-02-16 03:42:20 +0100

    diseño acabado de register y login

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,31 +1,36 @@
 <template>
-    <div class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-        <h2 class="mt-10 text-center text-4xl font-bold tracking-tight text-gray-900">Register</h2>
-        <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+<div class="flex items-center justify-center min-h-screen bg-gray-50">
+    <div class="bg-white rounded-lg shadow-lg p-8 w-full sm:w-96">
+        <h2 class="text-center text-4xl font-bold tracking-tight text-gray-900 mb-8">Registrar</h2>
         <form class="space-y-6" @submit.prevent="registerView">
             <div>
-                <label  class="block text-xl font-medium text-gray-900">Correo Electronico</label>
+                <label class="block text-xl font-medium text-gray-900">Correo Electrónico</label>
                 <div class="mt-2">
-                <input type="email" placeholder="Escribe tu Correo Eletronico" v-model="email" required class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+                    <input type="email" placeholder="Escribe tu Correo Electrónico" v-model="email" required
+                        class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:outline-offset-2 focus:outline-indigo-600 sm:text-sm" />
                 </div>
             </div>
             <div>
-            <div class="flex items-center justify-between">
-                <label for="password" class="block text-xl font-medium text-gray-900">Contraseña</label>
+                <div class="flex items-center justify-between">
+                    <label for="password" class="block text-xl font-medium text-gray-900">Contraseña</label>
+                </div>
+                <div class="mt-2">
+                    <input type="password" min="6" placeholder="Escribe tu Contraseña" v-model="password" required
+                        class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:outline-offset-2 focus:outline-indigo-600 sm:text-sm" />
+                </div>
             </div>
-            <div class="mt-2">
-                <input type="password" min="6" placeholder="Escribe tu Contraseña" v-model="password" required class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+            <div class="text-sm">
+                <router-link to="/login" class="font-semibold text-indigo-600 hover:text-indigo-500">¿Ya tienes cuenta?</router-link>
             </div>
-        </div>
-        <div class="text-sm">
-                    <router-link to="/login" class="font-semibold text-indigo-600 hover:text-indigo-500">Tienes cuenta?</router-link>
-        </div>
             <div class="flex items-center">
-                <button type="submit" class="flex w-40 justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Registarse</button>
+                <button type="submit"
+                    class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                    Registrarse
+                </button>
             </div>
         </form>
     </div>
-    </div>
+</div>
 </template>
 
 <script>
